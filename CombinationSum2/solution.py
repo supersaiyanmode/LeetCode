bf6fc24e3a501cf41c0180d73d92c80cb4c7cc7a
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2016-10-05 02:31:50 -0400

    More efficient.

--- CombinationSum2/solution.py
+++ CombinationSum2/solution.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+        candidates.sort()
         return list(self.proc(candidates, target, [], set()))
         
     def proc(self, available, target, used, cache):
@@ -12,14 +13,11 @@ class Solution(object):
             cache.add(tuple(used))
             yield used
         for i, x in enumerate(available):
-            if used and x < used[-1]:
-                continue
             if target == x:
                 if tuple(used + [x]) in cache:
                     continue
             if target >= x:
-                rem = [y for j, y in enumerate(available) if j != i]
+                rem = available[i+1:]
                 for item in self.proc(rem, target - x, used + [x], cache):
                     if item:
                         yield item
-                    
